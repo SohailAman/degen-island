d9847ff449ea8576eec72ec4e5f30c477a87890b
Maher Fayad <63962002+MaherFayad@users.noreply.github.com>
2024-03-26 22:51:04 +0300

    firebase deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <meta property="og:title" content="Falcrum^4" />
+      <meta property="og:description" content="Bridging the Talent-Tech Gap" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Falcrum^4" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png" />
+      </head>
       <body className={`${poppins.className} bg-[#ADD8EB]`}>{children}</body>
     </html>
   );
